8405b5f826093ea1d7174de133702db8ccae0639
Maribeth Calipo <maribethsc43@gmail.com>
2021-11-18 21:13:29 -0500

    Created Exercise.js Bonus.js files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// 3g
+import Bonus from "./bonus/Bonus";
 
+// 1d
+import Exercise from "./exercise/Exercise";
+//
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 1d */}
+      <Exercise />
+      {/* 3h */}
+      <Bonus />
     </div>
   );
+  
 }
 
 export default App;
